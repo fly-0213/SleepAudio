1550343af18fc195d85edcf1070bf2275f0c2f04
Yufan Liu <liuyufan20030213@gmail.com>
2026-05-25 14:01:12 +0800

    Initial Commit

---
+++ SleepAudio/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SleepAudio
+//
+//  Created by F.ly on 5/25/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ SleepAudioTests/SleepAudioTests.swift
@@ -0,0 +1,18 @@
+//
+//  SleepAudioTests.swift
+//  SleepAudioTests
+//
+//  Created by F.ly on 5/25/26.
+//
+
+import Testing
+
+struct SleepAudioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
